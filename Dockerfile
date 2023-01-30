11f98b81b2856697d4c145921b6e31107025df84
rohit-chavan-2151 <rc2151@gmail.com>
2023-01-30 18:07:35 +0530

    Fix app running issues for containers

--- Dockerfile
+++ Dockerfile
@@ -3,10 +3,12 @@ FROM node:14.21.2-alpine3.16
 ENV MONGO_DB_USERNAME=mongoadmin \
     MONGO_DB_PWD=rohit
 
-WORKDIR app
+WORKDIR /app
 
-COPY . /app
+COPY . .
 
 RUN npm install
+
+EXPOSE 3000
 
 CMD ["node","server.js"]
--- server.js
+++ server.js
@@ -23,10 +23,10 @@ app.get('/profile-picture', function (req, res) {
 })
 
 // use when starting application locally
-let mongoUrlLocal = 'mongodb://mongoadmin:rohit@localhost:27017'
+let mongoUrlLocal = 'mongodb://mongoadmin:rohit@mongodb'
 
 // use when starting application as docker container
-let mongoUrlDocker = 'mongodb://mongoadmin:rohit@mongodb'
+// let mongoUrlDocker = 'mongodb://mongoadmin:rohit@mongodb'
 
 // pass these options to mongo client connect request to avoid DeprecationWarning for current Server Discovery and Monitoring engine
 let mongoClientOptions = {
